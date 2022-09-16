e3edc6be4d85960b4f76e731f73556b8a509727d
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-09-16 02:15:37 +0000

    done the extra stuff

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright(c) 2022 Ioana Marinescu All rights reserved.
+// Created By : Ioana Marinescu
+
+// Date : Sept.14
+// prints with propers styling
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world! (with proper style!)";
+}
